30e24515db125bf2989d8fcef6f52163b278b0f2
Cyril Rohr <cyril.rohr@gmail.com>
2014-11-10 20:56:35 +0000

    Setup smtp settings initializer before packaging

--- bin/pkgr_before_precompile.sh
+++ bin/pkgr_before_precompile.sh
@@ -13,6 +13,23 @@ sed -i "s/host: localhost/host: <%= gitlab_url.host %>/" config/gitlab.yml
 sed -i "s/port: 80/port: <%= gitlab_url.port %>/" config/gitlab.yml
 sed -i "s/https: false/https: <%= gitlab_url.scheme == 'https' %>/" config/gitlab.yml
 
+cat > config/initializers/smtp_settings.rb << SMTP
+smtp_url = URI(ENV['SMTP_URL']) rescue nil
+if smtp_url
+  Gitlab::Application.config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    address: smtp_url.host,
+    port: smtp_url.port,
+    user_name: smtp_url.user,
+    password: smtp_url.password,
+    domain: smtp_url.path[1..-1],
+    authentication: :login,
+    enable_starttls_auto: true
+  }
+end
+SMTP
+
 # No need for config file. Will be taken care of by REDIS_URL env variable
 rm config/resque.yml
 
